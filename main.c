d2fce24cc14439d4e95c5930aca9f15ba353d17e
MikeCase <michael.e.case@gmail.com>
2022-08-12 23:56:26 -0500

    Added Space to jump to key help screen

--- main.c
+++ main.c
@@ -145,14 +145,15 @@ int main(void)
 
             DrawText(TextFormat("Player Health: %i", myPlayer.health), 600, 10, 20, GREEN);
 
-            DrawRectangle( 10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 250, 113, BLUE);
+            DrawRectangle( 10, 10, 250, 133, Fade(SKYBLUE, 0.5f));
+            DrawRectangleLines( 10, 10, 250, 133, BLUE);
 
             DrawText("Free 2d camera controls:", 20, 20, 10, BLACK);
             DrawText("- Right/Left to move Offset", 40, 40, 10, DARKGRAY);
             DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY);
             DrawText("- A / S to Rotate", 40, 80, 10, DARKGRAY);
             DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY);
+            DrawText("- Space to jump", 40, 120, 10, DARKGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
